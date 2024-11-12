9291e0d611aaa30f4ebfbac0275a3e7826422f23
Anine <679310@stud.hvl.no>
2024-11-12 23:37:21 +0100

    opgg 4

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,5 +1,6 @@
 package no.hvl.dat100.oppgave4;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
@@ -10,6 +11,20 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		PrintWriter writer;
+        String filsti = mappe + filnavn;
+        
+        try  {
+        	File file = new File(filsti);
+        	writer = new PrintWriter(file);
+            writer.write(samling.toString());  // Skriver samlingen til fil
+            writer.close();
+            return true;  // Returnerer true hvis filen ble skrevet uten problemer
+        } catch (FileNotFoundException e) {
+            System.err.println("Feil under skriving til fil: " + e.getMessage());
+              // Returnerer false ved feil
+        }
+        return false;
+		
 	}
 }
